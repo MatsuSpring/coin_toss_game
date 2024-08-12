c98957ba4102208d6645d2c2d0559cfac5087c63
harum <haru.mattsu0415@gmail.com>
2024-08-13 01:54:09 +0900

    toss_coin関数を実装した。coin_flagではなく、coin_imageのdataプロパティで表と裏を管理するようにした

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import flet as ft
+import random
+import time
 
 # 引数がTrueなら表、Falseなら裏のコインの相対パスを返す
 def get_coin_pass(flag):
@@ -8,11 +10,20 @@ def get_coin_pass(flag):
         return  "images/coin_tails.png"
 
 def main(page: ft.Page):
+
     # 中央揃え
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    # Trueなら表、Falseなら裏を表すものとする。
-    coin_flag = True
+    # 表と裏が入れ替わる回数をランダムにして、表と裏どちらかをランダムに結果として表示する
+    def toss_coin(e):
+        # 入れ替わる回数
+        swicth_time = random.randint(7, 15)
+        for i in range(swicth_time):
+            coin_image.data = not coin_image.data
+            coin_image.src = get_coin_pass(coin_image.data)
+            coin_image.update()
+            time.sleep(0.7)
+
     # アプリの説明のためのテキストコントロール
     title = ft.Text(
         value="コイントスゲーム",
@@ -24,9 +35,10 @@ def main(page: ft.Page):
     )
     # コインを表示するImageコントロール
     coin_image = ft.Image(
-        src=get_coin_pass(coin_flag),
+        src=get_coin_pass(True),
         width=200,
         height=200,
+        data=False  # このプロパティがTrueなら表、Falseなら裏とする。
     )
     # コイントスを実行するボタン
     toss_button = ft.ElevatedButton(
@@ -36,7 +48,8 @@ def main(page: ft.Page):
                 size=25
             ),
             padding=ft.padding.symmetric(10, 5)
-        )
+        ),
+        on_click=toss_coin
     )
 
     page.add(title, text, coin_image, toss_button)
